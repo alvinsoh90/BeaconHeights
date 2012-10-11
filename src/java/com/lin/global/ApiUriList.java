ffe01a3a2ed1e86e389325d9d4c225b5d362e4e7
Garreth <xy.peh.2010@sis.smu.edu.sg>
2012-10-11 15:48:07 +0800

    edited naming convention in apiurilist
    Signed-off-by: Garreth <xy.peh.2010@sis.smu.edu.sg>

--- src/java/com/lin/global/ApiUriList.java
+++ src/java/com/lin/global/ApiUriList.java
@@ -9,10 +9,10 @@ package com.lin.global;
  * @author Shamus
  */
 public class ApiUriList {
-    public static final String BaseURL = "http://127.0.0.1:3000/";
-    public static final String apiURL = "api/";
+    public static final String BASE_URL = "http://127.0.0.1:3000/";
+    public static final String API_URL = "api/";
     
     public static String getCheckLoginHashURI(String username){
-        return BaseURL + apiURL + "getUserHash/" + username;
+        return BASE_URL + API_URL + "getUserHash/" + username;
     }
 }
