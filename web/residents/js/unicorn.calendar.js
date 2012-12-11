d5b4e0881750461fa7445c0c4cfcc021e994959f
Fayanne <fayanne.foo.2010@smu.edu.sg>
2012-12-11 19:26:54 +0800

    Display dummy bookings
    Hardcoded some test bookings to display on calendar

--- web/residents/js/unicorn.calendar.js
+++ web/residents/js/unicorn.calendar.js
@@ -6,17 +6,8 @@ $(document).ready(function(){
 	
     unicorn.init();
 	
-    $('#add-event-submit').click(function(){
-        unicorn.add_event();
-    });
-	
-    $('#event-name').keypress(function(e){
-        if(e.which == 13) {	
-            unicorn.add_event();
-        }
-    });
-
-        
+  addEvents();//trigger adding of events
+  
 });
 
 unicorn = {	
