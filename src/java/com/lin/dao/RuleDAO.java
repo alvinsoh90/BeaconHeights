56bc4a4a7d0a445082e71c3cc1a1382cba9e4762
Shamus <shamusmohan@hotmail.com>
2012-12-12 19:39:55 +0800

    RuleDAO - Create and get done
    Signed-off-by: Shamus <shamusmohan@hotmail.com>

--- src/java/com/lin/dao/RuleDAO.java
+++ src/java/com/lin/dao/RuleDAO.java
@@ -4,8 +4,7 @@
  */
 package com.lin.dao;
 
-import com.lin.entities.Facility;
-import com.lin.entities.FacilityType;
+import com.lin.entities.*;
 import com.lin.utils.HttpHandler;
 import com.lin.global.ApiUriList;
 import com.lin.utils.BCrypt;
@@ -14,6 +13,7 @@ import com.lin.utils.json.JSONException;
 import com.lin.utils.json.JSONObject;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -27,7 +27,6 @@ import org.hibernate.Transaction;
  */
 public class RuleDAO {
 
-    ArrayList<FacilityType> typeList = null;
     Session session = null;
 
     public RuleDAO() {
@@ -38,59 +37,136 @@ public class RuleDAO {
         this.session = HibernateUtil.getSessionFactory().getCurrentSession();
     }
 
-    public ArrayList<FacilityType> retrieveAllFacilityTypes() {
+    public ArrayList<LimitRule> getAllLimitRule(int facilityTypeId){
         openSession();
-        typeList = new ArrayList<FacilityType>();
+        ArrayList<LimitRule> list = new ArrayList<LimitRule>();
         try {
             org.hibernate.Transaction tx = session.beginTransaction();
-            Query q = session.createQuery("from FacilityType");
-            typeList = (ArrayList<FacilityType>) q.list();
+            Query q = session.createQuery("from LimitRule WHERE FacilityType id = :id");
+            q.setString("id", facilityTypeId + "");
+            list = (ArrayList<LimitRule>) q.list();
         } catch (Exception e) {
             e.printStackTrace();
         }
-        return typeList;
-    }
-
-    public FacilityType createFacilityType(String name, String description) {
-
-        FacilityType facilityType = new FacilityType(name, description);
         
-        //line that says u put into DATABASE
-        return facilityType;
-
+        return list;
     }
-
-    public boolean deleteFacilityType(String name) {
-
-      //  FacilityType facilityType = typeList.remove(name);
-        boolean success = true;
-
-        //line that says u put into Objectify
-        return success;
-
-    }
-
-    public FacilityType updateFacilityType(String name, String description) {
-
-        FacilityType facilityType = new FacilityType(name, description);
-        session.update(facilityType);
-
-        //update user where id = id
-
-        return facilityType;
-    }
-
-    public FacilityType getFacilityType(String name) {
+    
+    public ArrayList<OpenRule> getAllOpenRule(int facilityTypeId){
         openSession();
-        ArrayList<FacilityType> typeList = new ArrayList<FacilityType>();
+        ArrayList<OpenRule> list = new ArrayList<OpenRule>();
         try {
             org.hibernate.Transaction tx = session.beginTransaction();
-            Query q = session.createQuery("from FacilityType where name ='" + name + "'");
-            typeList = (ArrayList<FacilityType>) q.list();
+            Query q = session.createQuery("from OpenRule WHERE FacilityType id = :id");
+            q.setString("id", facilityTypeId + "");
+            list = (ArrayList<OpenRule>) q.list();
         } catch (Exception e) {
             e.printStackTrace();
         }
-        return typeList.get(0);
-
+        
+        return list;
     }
+    
+    public ArrayList<CloseRule> getAllCloseRule(int facilityTypeId){
+        openSession();
+        ArrayList<CloseRule> list = new ArrayList<CloseRule>();
+        try {
+            org.hibernate.Transaction tx = session.beginTransaction();
+            Query q = session.createQuery("from CloseRule WHERE FacilityType id = :id");
+            q.setString("id", facilityTypeId + "");
+            list = (ArrayList<CloseRule>) q.list();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+        return list;
+    }
+    
+    public ArrayList<AdvanceRule> getAllAdvanceRule(int facilityTypeId){
+        openSession();
+        ArrayList<AdvanceRule> list = new ArrayList<AdvanceRule>();
+        try {
+            org.hibernate.Transaction tx = session.beginTransaction();
+            Query q = session.createQuery("from AdvanceRule WHERE FacilityType id = :id");
+            q.setString("id", facilityTypeId + "");
+            list = (ArrayList<AdvanceRule>) q.list();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+        return list;
+    }
+    
+    public LimitRule createLimitRule(int id, FacilityType facilityType, int sessions, int numberOfTimeframe, String timeframeType){
+        openSession();
+        LimitRule lRule = new LimitRule( id, facilityType, sessions, numberOfTimeframe, timeframeType);
+        
+        Transaction tx = null;
+        try {
+            tx = session.beginTransaction();
+            session.save("LimitRule",lRule);
+            tx.commit();
+            return lRule;
+        } catch (Exception e) {
+            e.printStackTrace();
+            if(tx!=null) tx.rollback();
+        }
+        //return null if failed
+        return null;
+    }
+    
+    public CloseRule createCloseRule(int id, FacilityType facilityType, Date startDate, Date endDate){
+        openSession();
+        CloseRule cRule = new CloseRule( id, facilityType, startDate, endDate);
+        
+        Transaction tx = null;
+        try {
+            tx = session.beginTransaction();
+            session.save("CloseRule",cRule);
+            tx.commit();
+            return cRule;
+        } catch (Exception e) {
+            e.printStackTrace();
+            if(tx!=null) tx.rollback();
+        }
+        //return null if failed
+        return null;
+    }
+    
+    public OpenRule createOpenRule(int id, FacilityType facilityType, int dayOfWeek, int startTime, int endTime){
+        openSession();
+        OpenRule oRule = new OpenRule( id, facilityType, dayOfWeek, startTime, endTime);
+        
+        Transaction tx = null;
+        try {
+            tx = session.beginTransaction();
+            session.save("OpenRule",oRule);
+            tx.commit();
+            return oRule;
+        } catch (Exception e) {
+            e.printStackTrace();
+            if(tx!=null) tx.rollback();
+        }
+        //return null if failed
+        return null;
+    }
+    
+    public AdvanceRule createAdvanceRule(int id, FacilityType facilityType, int minDays, int maxDays){
+        openSession();
+        AdvanceRule aRule = new AdvanceRule( id, facilityType, minDays, maxDays);
+        
+        Transaction tx = null;
+        try {
+            tx = session.beginTransaction();
+            session.save("AdvanceRule",aRule);
+            tx.commit();
+            return aRule;
+        } catch (Exception e) {
+            e.printStackTrace();
+            if(tx!=null) tx.rollback();
+        }
+        //return null if failed
+        return null;
+    }
+    
 }
