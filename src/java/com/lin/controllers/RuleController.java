5b7536466318183f7038afc8c1b395415d83d21f
Kevin Quah <kevin.quah@gmail.com>
2012-12-26 17:30:06 +0800

    RuleController redux
    Please test and see if it functions. Still without limit logic, figuring it out.
    
    Signed-off-by: Kevin Quah <kevin.quah@gmail.com>

--- src/java/com/lin/controllers/RuleController.java
+++ src/java/com/lin/controllers/RuleController.java
@@ -4,9 +4,11 @@
  */
 package com.lin.controllers;
 
+import com.lin.dao.BookingDAO;
 import com.lin.dao.FacilityDAO;
 import com.lin.dao.FacilityTypeDAO;
 import com.lin.dao.RuleDAO;
+import com.lin.dao.UserDAO;
 import com.lin.entities.*;
 import java.util.ArrayList;
 import java.util.Date;
@@ -18,58 +20,114 @@ import java.util.Set;
  */
 public class RuleController {
 
-    public ArrayList<String> isFacilityAvailable(int userID, int facilityID, Date startBookingTime, Date endBookingTime){
+    public ArrayList<String> isFacilityAvailable(int userID, int facilityID, Date startBookingTime, Date endBookingTime) {
         ArrayList<String> errorMsg = new ArrayList<String>();
-        
+
+        UserDAO uDAO = new UserDAO();
+        User user = uDAO.getUser(userID);
+        BookingDAO bDAO = new BookingDAO();
+        ArrayList<Booking> userBookingList = bDAO.getUserBookings(userID);
+
+
         RuleDAO rDAO = new RuleDAO();
         FacilityTypeDAO tDAO = new FacilityTypeDAO();
         FacilityDAO fDAO = new FacilityDAO();
-        
+
         Facility facility = fDAO.getFacility(facilityID);
         FacilityType facilityType = facility.getFacilityType();
-        
+
         Set openRuleSet = facilityType.getOpenRules();
         Set closeRuleSet = facilityType.getCloseRules();
         Set limitRuleSet = facilityType.getLimitRules();
         Set advanceRuleSet = facilityType.getAdvanceRules();
-        
-        for(Object o : openRuleSet){
+
+        for (Object o : openRuleSet) {
             OpenRule openRule = (OpenRule) o;
-            if (openRule.getDayOfWeek()==startBookingTime.getDay()){
+            if (openRule.getDayOfWeek() == startBookingTime.getDay()) {
                 Date openStart = openRule.getStartTime();
-                if (openStart.getHours()>startBookingTime.getHours()){
+                if (openStart.getHours() > startBookingTime.getHours()) {
                     errorMsg.add("Unable to book before facility opens!");
-                } else if (openStart.getMinutes()> startBookingTime.getMinutes()){
+                } else if (openStart.getMinutes() > startBookingTime.getMinutes()) {
                     errorMsg.add("Unable to book before facility opens! Check to the minute.");
                 }
-                
+
                 Date openEnd = openRule.getEndTime();
-                if (openEnd.getHours()< endBookingTime.getHours()){
+                if (openEnd.getHours() < endBookingTime.getHours()) {
                     errorMsg.add("Unable to book after facility closes!");
-                } else if (openEnd.getMinutes()< endBookingTime.getMinutes()){
+                } else if (openEnd.getMinutes() < endBookingTime.getMinutes()) {
                     errorMsg.add("Unable to book after facility closes! Check to the minute.");
                 }
-                
-            }
-            
-            
-        }
-        
-        for (Object o : closeRuleSet){
-            CloseRule closeRule = (CloseRule) o;
-            
-            //if the start of booking falls between the start of the closed time and end of it
-            if (closeRule.getStartDate().before(startBookingTime) && closeRule.getEndDate().after(startBookingTime)){
-                errorMsg.add("Sorry, the facility is closed on that day.");
-            //if the end of the booking falls between the start of the closed time and end of it
-            } else if (closeRule.getStartDate().before(endBookingTime) && closeRule.getEndDate().after(endBookingTime)){
-                errorMsg.add("Sorry, the facility is closed on that day.");
-            }
-        }
-        
-        return errorMsg;
-               
-        
-    }
 
+            }
+
+
+        }
+
+        for (Object o : closeRuleSet) {
+            CloseRule closeRule = (CloseRule) o;
+
+            //if the start of booking falls between the start of the closed time and end of it
+            if (closeRule.getStartDate().before(startBookingTime) && closeRule.getEndDate().after(startBookingTime)) {
+                errorMsg.add("Sorry, the facility is closed on that day.");
+                //if the end of the booking falls between the start of the closed time and end of it
+            } else if (closeRule.getStartDate().before(endBookingTime) && closeRule.getEndDate().after(endBookingTime)) {
+                errorMsg.add("Sorry, the facility is closed on that day.");
+            }
+        }
+
+        for (Object o : limitRuleSet) {
+            LimitRule limitRule = (LimitRule) o;
+            String timeFrametype = limitRule.getTimeframeType();
+            int numberOfTimeframe = limitRule.getNumberOfTimeframe();
+            int count = 0;
+            if (timeFrametype.equals("DAY")) {
+                for (Booking booking : userBookingList) {
+                    Date bookingDate = booking.getStartDate();
+                    if (bookingDate.getYear() == startBookingTime.getYear()) {
+
+                        Facility bookingFacility = booking.getFacility();
+                        FacilityType bookingFacilityType = bookingFacility.getFacilityType();
+                        if (bookingFacilityType.equals(facilityType)) {
+                            count++;
+                        }
+
+                    }
+                }
+
+            } else if (timeFrametype.equals("MONTH")) {
+            } else if (timeFrametype.equals("YEAR")) {
+            }
+
+            //basic check booking code without time
+
+
+        }
+        
+        for (Object o : advanceRuleSet){
+            AdvanceRule advanceRule = (AdvanceRule) o;
+            Date currentDate = new Date();
+            long currentTime = currentDate.getTime();
+            long startTime = startBookingTime.getTime();
+            long daysBetween = getMillisecondsToDay(startTime-currentTime);
+            if (daysBetween > advanceRule.getMaxDays()){
+                errorMsg.add("Sorry, the facility allows you to book a maximum of " + advanceRule.getMaxDays() + " in advance.");
+            } else if (daysBetween < advanceRule.getMinDays()){
+                errorMsg.add("Sorry, you need to book the facility at least "+ advanceRule.getMinDays() + " in advance." );
+            }
+          
+            
+        }
+
+        return errorMsg;
+
+
+    }
+    
+    private long getDayToMilliseconds(int i){
+        return i*1000*60*60*24;
+    }
+    
+    private long getMillisecondsToDay(long i){
+        return i/1000/60/60/24;
+    }
 }
