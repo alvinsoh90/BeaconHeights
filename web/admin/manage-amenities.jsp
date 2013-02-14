81fec59b58ec62e0946db4c3c5f29a0689f8609e
Garreth <xy.peh.2010@sis.smu.edu.sg>
2013-02-15 04:07:12 +0800

    fixed manage amenities validation
    Signed-off-by: Garreth <xy.peh.2010@sis.smu.edu.sg>

--- web/admin/manage-amenities.jsp
+++ web/admin/manage-amenities.jsp
@@ -19,13 +19,13 @@
         <link rel="stylesheet" href="/css/chosen.css" />
 
         <link href="/datatables/media/css/jquery.dataTables_themeroller.css" rel="stylesheet">
-                <script src="/js/custom/lin.manageamenities.js"></script>
+                
 
         <script src="js/jquery.js"></script>
         <script type="text/javascript" charset="utf-8" src="/datatables/media/js/jquery.dataTables.js"></script>
         <script src="/js/jquery.chosen.js"></script>
                 <script src="../js/jquery.validate.js"></script>
-
+<script src="/js/custom/lin.manageamenities.js"></script> 
 
         <!--[if lt IE 9]>
           <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
@@ -46,7 +46,8 @@
             // Init an array of all facilities shown on this page
             var amenityList = [];
             
-            function populateEditAmenityModal(aID){ 
+            function populateEditAmenityModal(aID){
+                setupValidation();
                 amenityList.forEach(function(amenity){
                     if(amenity.id == aID){
                         $("#amenityEditLabel").text(amenity.name);
@@ -60,6 +61,7 @@
                         $("#edit_cat").val(amenity.amenityCategory.name);
                     }
                 });
+                
                 
             }
             
@@ -113,7 +115,7 @@
                                 <div class="control-group ${errorStyle}">
                                     <label class="control-label">Unit No</label>
                                     <div class="controls">
-                                        <stripes:text id="edit_unitNo" name="unitNo"/>
+                                        <stripes:text id="edit_unitNo" name="unitno"/>
                                     </div>
                                 </div>
                                 <div class="control-group ${errorStyle}">
@@ -236,7 +238,7 @@
                 <h3>Create New Amenity</h3>
             </div>
             <div class="modal-body">
-                <stripes:form class="form-horizontal" beanclass="com.lin.general.admin.ManageAmenityBean" name="amenity_validate" id="amenity_validate">
+                <stripes:form class="form-horizontal" beanclass="com.lin.general.admin.ManageAmenityBean" name="amenity_validate2" id="amenity_validate2">
                     <div class="control-group ${errorStyle}">
                         <label class="control-label">Name</label>
                         <div class="controls">
@@ -310,6 +312,9 @@
                 }
             });
         </script>
-
+        <script>
+            
+        
+        </script>
     </body>
 </html>
--- web/js/custom/lin.manageamenities.js
+++ web/js/custom/lin.manageamenities.js
@@ -1,58 +1,122 @@
+function setupValidation() {
+    //for edit amenity modal
+                $("#amenity_validate").validate({
+                    rules:{
+                        name:{
+                            required: true,
+                            minlength:1,
+                            maxlength:50
+                        },
+                        description:{
+                            required: true,
+                            minlength:1,
+                            maxlength:100
+                        },
+                        unitno:{
+                            required:true,
+                            minlength:1,
+                            maxlength:7,
+                            digits:true
+                        },
+                        streetName:{
+                            required: true,
+                            minlength:1,
+                            maxlength:60
+                        },
+                        postalCode:{
+                            required: true,
+                            digits: true,
+                            minlength:6,
+                            maxlength:6
+                        },
+                        contactNo:{
+                            required: true,
+                            digits: true,
+                            minlength:8,
+                            maxlength:8
+                        },
 
-window.onload = function() {
-	
-    $("#amenity_validate").validate({
-        rules:{
-            name:{
-                required: true,
-                minlength:1,
-                maxlength:50
-            },
-            description:{
-                required: true,
-                minlength:1,
-                maxlength:100
-            },
-            unitNo:{
-                required:true,
-                minlength:1,
-                maxlength:7
-            },
-            streetName:{
-                required: true,
-                minlength:1,
-                maxlength:60
-            },
-            postalCode:{
-                required: true,
-                digit: true,
-                minlength:6,
-                maxlength:6
-            },
-            contactNo:{
-                required: true,
-                digit: true,
-                minlength:8,
-                maxlength:8
-            },
-            
-            category:{
-                required: true
-            }
-            
-        },
-        errorClass: "help-inline",
-        errorElement: "span",
-        highlight:function(element, errorClass, validClass) {
-            $(element).parents('.control-group').addClass('error');
-        },
-        unhighlight: function(element, errorClass, validClass) {
-            $(element).parents('.control-group').removeClass('error');
-            $(element).parents('.control-group').addClass('success');
-        }
-    })
-   
+                        category:{
+                            required: true
+                        }
 
-};
+                    },
+                   submitHandler: function(form) {
+                    alert("Test");
+
+                    },
+                    errorClass: "help-inline",
+                    errorElement: "span",
+                    highlight:function(element, errorClass, validClass) {
+                        $(element).parents('.control-group').removeClass('success');
+                        $(element).parents('.control-group').addClass('error');
+                    },
+                    unhighlight: function(element, errorClass, validClass) {
+                        $(element).parents('.control-group').removeClass('error');
+                        $(element).parents('.control-group').addClass('success');
+                    }
+                });
+
+
+            };
         
-       
+$(document).ready(function(){    
+    //for create amenity modal
+    $("#amenity_validate2").validate({
+                    rules:{
+                        name:{
+                            required: true,
+                            minlength:1,
+                            maxlength:50
+                        },
+                        description:{
+                            required: true,
+                            minlength:1,
+                            maxlength:100
+                        },
+                        unitNo:{
+                            required:true,
+                            minlength:1,
+                            maxlength:7,
+                            digits:true
+                        },
+                        streetName:{
+                            required: true,
+                            minlength:1,
+                            maxlength:60
+                        },
+                        postalCode:{
+                            required: true,
+                            digits: true,
+                            minlength:6,
+                            maxlength:6
+                        },
+                        contactNo:{
+                            required: true,
+                            digits: true,
+                            minlength:8,
+                            maxlength:8
+                        },
+
+                        category:{
+                            required: true
+                        }
+
+                    },
+                   submitHandler: function(form) {
+                    alert("Test");
+
+                    },
+                    errorClass: "help-inline",
+                    errorElement: "span",
+                    highlight:function(element, errorClass, validClass) {
+                        $(element).parents('.control-group').removeClass('success');
+                        $(element).parents('.control-group').addClass('error');
+                    },
+                    unhighlight: function(element, errorClass, validClass) {
+                        $(element).parents('.control-group').removeClass('error');
+                        $(element).parents('.control-group').addClass('success');
+                    }
+                });
+});
+
