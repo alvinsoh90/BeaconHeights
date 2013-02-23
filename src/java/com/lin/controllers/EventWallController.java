4e47bb3d7f1cd918578aa37706368db7beb0c061
Fayanne <fayanne.foo.2010@smu.edu.sg>
2013-02-24 03:39:42 +0800

    EventWallController added

--- src/java/com/lin/controllers/EventWallController.java
+++ src/java/com/lin/controllers/EventWallController.java
@@ -4,31 +4,39 @@
  */
 package com.lin.controllers;
 
+import com.lin.dao.EventCommentDAO;
 import com.lin.dao.EventDAO;
 import com.lin.dao.UserDAO;
-import com.lin.entities.Comment;
+import com.lin.entities.EventComment;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Date;
 
 /**
  *
  * @author fayannefoo
  */
 public class EventWallController {
-    
-    UserDAO uDAO = new UserDAO();
-    //EventCommentDAO cDAO = new EventCommentDAO();
-    EventDAO eDAO = new EventDAO();
-    
-    public void addCommentOnEvent(int posterId, int postId, String content){
 
+    UserDAO uDAO = new UserDAO();
+    EventCommentDAO cDAO = new EventCommentDAO();
+    EventDAO eDAO = new EventDAO();
+
+    public void addCommentOnEvent(int eventId, int posterId, String content) {
+        
+        EventComment eComment = new EventComment(
+                eDAO.getEvent(eventId),
+                uDAO.getUser(posterId),
+                content,
+                new Date(),
+                false);
+
+        cDAO.createEventComment(eComment);
     }
-    
-    public ArrayList<Comment> getCommentsForEventSortedByDate(int postId){
-        //ArrayList<Comment> list = cDAO.retrieveCommentsForPost(postId);
+
+    public ArrayList<EventComment> getCommentsForEventSortedByDate(int eventId) {
+        ArrayList<EventComment> list = cDAO.getAllCommentsForEvent(eventId);
         //Collections.sort(list);
-        //return list;  
-        return null;
+        return list;  
     }
-    
 }
