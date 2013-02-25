0ac6f5c8db3f0978d24b407e52fb67aeb8faf851
Shamus <shamusmohan@hotmail.com>
2013-02-26 01:46:26 +0800

    Added required methods to eventDAO (untested, passing on to XY to use)
    Signed-off-by: Shamus <shamusmohan@hotmail.com>

--- src/java/com/lin/dao/EventDAO.java
+++ src/java/com/lin/dao/EventDAO.java
@@ -4,9 +4,7 @@
  */
 package com.lin.dao;
 
-import com.lin.entities.Booking;
-import com.lin.entities.Event;
-import com.lin.entities.User;
+import com.lin.entities.*;
 import com.lin.utils.HibernateUtil;
 import java.util.ArrayList;
 import java.util.Date;
@@ -48,6 +46,23 @@ public class EventDAO {
         //return null if failed
         return null;
     }
+    
+    public Event createEvent(Event e) {
+        Transaction tx = null;
+        try {
+            tx = session.beginTransaction();
+            session.save("Event", e);
+            tx.commit();
+            return e;
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            if (tx != null) {
+                tx.rollback();
+            }
+        }
+        //return null if failed
+        return null;
+    }
 
     public Event getEvent(int id) {
         return (Event) session.get(Event.class, id);
@@ -58,7 +73,7 @@ public class EventDAO {
         ArrayList<Event> list = new ArrayList<Event>();
         try {
             org.hibernate.Transaction tx = session.beginTransaction();
-            Query q = session.createQuery("from Event as e join fetch e.user join fetch e.booking");
+            Query q = session.createQuery("from Event as e join fetch e.user join fetch e.booking order by e.id DESC");
             list = (ArrayList<Event>) q.list();
             tx.commit();
         } catch (Exception e) {
@@ -73,13 +88,65 @@ public class EventDAO {
         ArrayList<Event> list = new ArrayList<Event>();
         try {
             org.hibernate.Transaction tx = session.beginTransaction();
-            Query q = session.createQuery("from Event");
+            Query q = session.createQuery("from Event order by e.id DESC");
             list = (ArrayList<Event>) q.list();
             tx.commit();
         } catch (Exception e) {
             e.printStackTrace();
         }
 
+        return list;
+    }
+    
+    public ArrayList<Event> retrieveEventsWithLimit(int limit) {
+        openSession();
+        ArrayList<Event> list = new ArrayList<Event>();
+        try {
+            org.hibernate.Transaction tx = session.beginTransaction();
+            Query q = session.createQuery("from Event as e join fetch e.user join fetch e.booking order by e.id DESC")
+                    .setMaxResults(limit);
+            
+            list = (ArrayList<Event>) q.list();
+            
+            tx.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return list;
+    }
+    public ArrayList<Event> retrieveEventsWithOffset(int offsetSize, int nextChunkSize) {
+        openSession();
+        ArrayList<Event> list = new ArrayList<Event>();
+        try {
+            org.hibernate.Transaction tx = session.beginTransaction();
+            Query q = session.createQuery("from Event as e join fetch e.user join fetch e.booking order by e.id DESC");
+            
+            q.setFirstResult(offsetSize);
+            q.setMaxResults(nextChunkSize);
+            
+            list = (ArrayList<Event>) q.list();
+            
+            tx.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return list;
+    }
+    
+    public ArrayList<Event> retrieveEventByUserId(int user_id) {
+        openSession();
+        ArrayList<Event> list = new ArrayList<Event>();
+        try {
+            org.hibernate.Transaction tx = session.beginTransaction();
+            Query q = session.createQuery("from Event where id = :id");
+            q.setString("id", user_id + "");
+            list = (ArrayList<Event>) q.list();
+            tx.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         return list;
     }
 
@@ -129,5 +196,232 @@ public class EventDAO {
             }
         }
         return false;
+    }
+    
+    public EventLike likeEvent(EventLike eventLike){
+        openSession();
+        Transaction tx = null;
+        
+        try {
+            tx = session.beginTransaction();
+            session.save("EventLike", eventLike);
+            session.flush();
+            
+            tx.commit();
+            return eventLike;
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+            if (tx != null) {
+                tx.rollback();
+            }
+        }
+        return null;
+    }
+    
+    public EventLike getEventLike(int id){        
+        EventLike el = new EventLike();
+        openSession();
+        
+        try {
+            org.hibernate.Transaction tx = session.beginTransaction();
+            Query q = session.createQuery("from EventLike where id = :id");
+            q.setInteger("id", id);
+            el = (EventLike) q.uniqueResult();
+            tx.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+        return el;
+    }
+    
+    public boolean unlikeEvent(int userId, int eventId) {
+        openSession();
+        Transaction tx = null;
+        int rowCount = 0;
+        
+        try {
+            tx = session.beginTransaction();
+            String hql = "delete from EventLike as el "
+                    + "where el.event.eventId = :eid "
+                    + "and el.user.userId = :uid";
+            Query query = session.createQuery(hql);
+            query.setInteger("eid", eventId);
+            query.setInteger("uid", userId);
+            rowCount = query.executeUpdate();
+            tx.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            if (tx != null) {
+                tx.rollback();
+            }
+        }
+        
+        if (rowCount > 0) {
+            return true;
+        } else {
+            return false;
+        }   
+    }
+    
+    public boolean hasUserLikedEvent(int eventId, int userId){
+        
+        ArrayList<EventLike> eventLikeList = new ArrayList<EventLike>();
+        
+        openSession();
+        try {
+            org.hibernate.Transaction tx = session.beginTransaction();
+            Query q = session.createQuery("from EventLike as el "
+                    + "where el.event.eventId = :id "
+                    + "and el.user.userId = :uid");
+            q.setInteger("id", eventId);
+            q.setInteger("uid", userId);
+            
+            eventLikeList = (ArrayList<EventLike>) q.list();
+            tx.commit();
+            
+            return !eventLikeList.isEmpty();
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+        return false;
+    }
+    
+    public ArrayList<EventLike> getEventLikesByEventId(int eventId) {
+        openSession();
+        ArrayList<EventLike> eventLikeList = new ArrayList<EventLike>();
+        
+        try {
+            org.hibernate.Transaction tx = session.beginTransaction();
+            Query q = session.createQuery("from EventLike as el "
+                    + "where el.event.eventId = :id");
+            
+            q.setInteger("id", eventId);
+            eventLikeList = (ArrayList<EventLike>) q.list();
+            
+            tx.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+        return eventLikeList;
+    }
+    
+    public boolean flagEventInappropriate(EventInappropriate ei) {
+        openSession();
+        Transaction tx = null;
+        
+        try {
+            tx = session.beginTransaction();
+            session.save("EventInappropriate", ei);
+            session.flush();
+            
+            tx.commit();
+            return true;
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+            if (tx != null) {
+                tx.rollback();
+            }
+        }
+        return false;
+    }
+    
+    public boolean unFlagEventInappropriate(int userId, int eventId) {
+        openSession();
+        Transaction tx = null;
+        int rowCount = 0;
+        
+        try {
+            tx = session.beginTransaction();
+            String hql = "delete from EventInappropriate as e "
+                    + "where e.event.eventId = :eid "
+                    + "and e.user.userId = :uid";
+            Query query = session.createQuery(hql);
+            query.setInteger("eid", eventId);
+            query.setInteger("uid", userId);
+            rowCount = query.executeUpdate();
+            tx.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            if (tx != null) {
+                tx.rollback();
+            }
+        }
+        
+        if (rowCount > 0) {
+            return true;
+        } else {
+            return false;
+        }   
+    }
+    
+    public EventInappropriate getEventInappropriate(int userId, int eventId){
+        openSession();
+        EventInappropriate ei = new EventInappropriate();
+        
+        try {
+            org.hibernate.Transaction tx = session.beginTransaction();
+            Query q = session.createQuery("from EventInappropriate as e "
+                    + "where e.event.eventId = :eid "
+                    + "and e.user.userId = :uid");
+            
+            q.setInteger("pid", eventId);
+            q.setInteger("uid", userId);
+            ei = (EventInappropriate) q.uniqueResult();
+            
+            tx.commit();
+            return ei;
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+        return null;
+    }
+    
+    public boolean addEventInvite(EventInvite eventInvite) {
+        openSession();
+        Transaction tx = null;
+        
+        try {
+            tx = session.beginTransaction();
+            session.save("EventInvite", eventInvite);
+            session.flush();
+            
+            tx.commit();
+            return true;
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+            if (tx != null) {
+                tx.rollback();
+            }
+        }
+        return false;
+    }
+    
+    public ArrayList<EventInvite> getEventInvitesByEventId(int eventId) {
+        openSession();
+        ArrayList<EventInvite> eventInviteList = new ArrayList<EventInvite>();
+        
+        try {
+            org.hibernate.Transaction tx = session.beginTransaction();
+            Query q = session.createQuery("from EventInvite as ei "
+                    + "where ei.event.evenId = :id");
+            
+            q.setInteger("id", eventId);
+            eventInviteList = (ArrayList<EventInvite>) q.list();
+            
+            tx.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+        return eventInviteList;
     }
 }
